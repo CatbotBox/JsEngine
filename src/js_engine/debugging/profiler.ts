786e34a00f440cbc16da8bb385a33f11a88a0de4
Xavier Tay <45095604+CatbotBox@users.noreply.github.com>
2025-10-14 15:56:03 +0800

    added profiling options

---
+++ src/js_engine/debugging/profiler.ts
@@ -0,0 +1,37 @@
+﻿import {performance} from "node:perf_hooks";
+import {SimpleProfiler} from "./simpleProfiler";
+
+export class Profiler extends SimpleProfiler {
+
+    private readonly _name: string;
+    private readonly _creationTime: number;
+    private _children: Map<string, Profiler> = new Map<string, Profiler>();
+
+    public get creationTime(): number {
+        return this._creationTime;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public get children(): Map<string, Profiler> {
+        return this._children;
+    }
+
+    public constructor(name: string) {
+        super();
+        this._name = name;
+        this._creationTime = performance.now();
+    }
+
+    public getChapter(name: string): Profiler {
+        const chp = this._children.get(name);
+        if (chp) return chp
+        const newChapter = new Profiler(name)
+        this._children.set(name, newChapter);
+        return newChapter;
+    }
+}
+
+
---
+++ src/js_engine/debugging/simpleProfiler.ts
@@ -0,0 +1,28 @@
+﻿import {AverageStat} from "../datatypes";
+import {performance} from "node:perf_hooks";
+
+export class SimpleProfiler {
+    private avg: AverageStat = new AverageStat(20);
+
+    private lastStartTime: number = -1;
+
+    public get Average() {
+        return this.avg.getAvg();
+    }
+
+    public start() {
+        if (this.lastStartTime != -1) throw new Error("Previous Profile has not ended");
+        this.lastStartTime = performance.now();
+
+        return {
+            [Symbol.dispose]: this.end,
+            [Symbol.asyncDispose]: this.end,
+        }
+    }
+
+    public end() {
+        const timeTaken = performance.now() - this.lastStartTime;
+        this.lastStartTime = -1;
+        this.avg.add(timeTaken);
+    }
+}
